76f47645ed8b3e0176d8300bfab9fff81659e46f
Auston S F <auzton7@gmail.com>
2026-08-11 22:26:50 +0530

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # Consider this as a 1D matrix for applying binary search and to check if target is present we use the formula to find rows and columns
+        m = len(matrix)
+        n = len(matrix[0])
+
+        total = m * n
+        l = 0
+        r = total - 1
+
+        while l <= r:
+            m = l + ((r - l) // 2)
+            row = m // n #Formula
+            col = m % n #Formula
+            if target == matrix[row][col]:
+                return True
+            elif target > matrix[row][col]:
+                l = m + 1
+            else:
+                r = m - 1
+        return False
